6678e517c690d7f14ba1f280c153d520e70e4825
krititwa <krititwaray97@gmail.com>
2026-02-01 11:02:52 +0530

    thirdPattern

--- Warm Up/starPatters.js
+++ Warm Up/starPatters.js
@@ -49,5 +49,34 @@ function secondPattern(n) {
     return pattern
 }
 
-// let result = secondPattern(10);
-// console.log(result);
+/* let result = secondPattern(10);
+console.log(result); */
+
+
+function thirdPattern(n) {
+
+    /* 
+        1    
+        1 2   
+        1 2  3
+        1 2  3 4
+        1 2  3 4 5
+    */
+
+    let pattern = ""
+
+    for (let i = 0; i < n; i++) {
+        let row = "";
+        for (let j = 0; j <= i; j++) {
+
+            row += j + 1;
+
+        }
+        pattern += row + "\n"
+
+    }
+    return pattern
+}
+
+/* let result = thirdPattern(5);
+console.log(result); */
