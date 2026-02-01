b06f094a892ae0ac942dbd26766da3306737049e
krititwa <krititwaray97@gmail.com>
2026-02-01 10:49:45 +0530

    secondPattern

--- Warm Up/starPatters.js
+++ Warm Up/starPatters.js
@@ -26,3 +26,28 @@ function firstPattern(n) {
 
 /* let pattern = firstPattern(4)
 console.log(pattern); */
+
+
+function secondPattern(n) {
+    let pattern = ""
+    /* 
+        *
+        ** 
+        ***  
+        **** 
+    */
+
+    for (let i = 0; i < n; i++) {
+        let row = ""
+        for (let j = 0; j <= i; j++) {
+
+            row += "*"
+        }
+        pattern += row + '\n'
+
+    }
+    return pattern
+}
+
+// let result = secondPattern(10);
+// console.log(result);
