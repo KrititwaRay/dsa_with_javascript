ee95aafa5ab29a5fca4babee26f52cf26fa40771
krititwa <krititwaray97@gmail.com>
2026-02-02 21:21:52 +0530

    fifthPattern done

--- Warm Up/index.js
+++ Warm Up/index.js
@@ -103,3 +103,30 @@ function forthPattern(n) {
 
 // let result = forthPattern(5)
 // console.log(result);
+function fifthPattern(n){
+
+     /*  12345
+         1234
+         123
+         12
+         1
+     */
+
+    let pattern = "";
+
+    for(let i=0; i<n;i++){
+        let row = ""
+        for(let j=0; j<n - i; j++){
+            row += j +1
+
+        }
+        pattern += row + '\n'
+    }
+
+
+    return pattern;
+
+}
+
+// let result = fifthPattern(5)
+// console.log(result);
