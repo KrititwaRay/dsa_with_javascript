10ed20d21649235f71a52fa32ef9f01d1cf7b694
krititwa <krititwaray97@gmail.com>
2026-02-03 22:34:24 +0530

    remove code from index.js

--- Warm Up/index.js
+++ Warm Up/index.js
@@ -34,20 +34,3 @@
 
 */
 
-
-let removeDuplicates = function(nums) {
-
-    let x = 0;
-    for(let i = 0; i<nums.length; i++){
-        if(nums[i] > nums[x]){
-            x = x +1
-            nums[x] = nums[i]
-        }
-    }
-    console.log(nums);
-    return x + 1;   
-};
-
-
-let nums = [0,0,1,1,1,2,2,3,3,4]
-console.log(removeDuplicates(nums));
