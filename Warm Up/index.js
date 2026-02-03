21f209f61235ece6d7399f5a346bc37a95000c48
krititwa <krititwaray97@gmail.com>
2026-02-03 22:23:00 +0530

    tenthPattern

--- Warm Up/index.js
+++ Warm Up/index.js
@@ -71,6 +71,17 @@ function tenthPattern(n){
 
 let removeDuplicates = function(nums) {
 
+    let x = 0;
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i] > nums[x]){
+            x = x +1
+            nums[x] = nums[i]
+        }
+    }
+
+    console.log(nums);
+    return x + 1
+
  
     
 };
--- Warm Up/starPatters.js
+++ Warm Up/starPatters.js
@@ -253,3 +253,35 @@ function ninthPattern(n){
 
 // let result = ninthPattern(5)
 // console.log(result);
+
+
+
+function tenthPattern(n){
+
+
+        //     *
+        //    ***
+        //   *****
+        //  *******
+        // *********
+
+
+    let pattern = ""
+    for (let i = 0; i < n; i++) {
+
+        let row = ""
+        for (let j = 0; j < n - i - 1; j++) {
+            row += " "
+        }
+        for (let k = 0; k < 2 * i + 1; k++) {
+            row += "*";
+        }
+        pattern += row + "\n"
+    }
+
+    return pattern
+
+}
+
+// let result = tenthPattern(5)
+// console.log(result);
