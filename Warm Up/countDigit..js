031dd5aaa129300d5a8559dfe9fb5c742a3a3379
krititwa <krititwaray97@gmail.com>
2026-02-05 21:56:51 +0530

    countDigit modified

---
+++ Warm Up/countDigit..js
@@ -0,0 +1,18 @@
+function countDigit(number) {
+
+    if (number == 0) return 1
+
+    // if negative number make positive
+    number = Math.abs(number)
+
+    let count = 0;
+    while (number > 0) {
+        number = Math.floor(number / 10);
+        count++;
+    }
+    return count
+}
+
+let number = -1
+let result = countDigit(number);
+console.log(result);
