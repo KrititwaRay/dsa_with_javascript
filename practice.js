7bb583bb0444ebf1a1e57f814707493c2015e8b5
krititwa <krititwaray97@gmail.com>
2026-03-08 13:09:43 +0530

    creating linkedlist using class

--- practice.js
+++ practice.js
@@ -1,25 +1,88 @@
-function bubbleSort(arr) {
-    // console.log(arr.sort());
-    let n = arr.length - 1
-    for (let i = 0; i < n; i++) {
-        let isSort = false;
-        for (let j = 0; j < n - i; j++) {
-
-            if (arr[j] > arr[j + 1]) {
-                arr[j] = arr[j] ^ arr[j + 1]
-                arr[j + 1] = arr[j] ^ arr[j + 1]
-                arr[j] = arr[j] ^ arr[j + 1]
-
-                isSort = true
-            }
-        }
-        if (!isSort) break
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
     }
-    return arr
-    /* 
-        Time Complexity = O(n^2)
-        Space Complexity = O(1)
-    */
 }
-let result = bubbleSort([4, 9, 0, 2, 8, 7, 1]);
-console.log(result);
+
+
+class MyLinkedList {
+    constructor(){
+        this.head = null;
+        this.size = 0;
+
+    }
+
+    addToHead(val){
+        let newNode = new Node(val);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.size++
+        
+    }
+
+    addToTail(val){
+        let newNode = new Node(val);
+
+        if(this.size == 0){
+            this.head = newNode;
+            this.size++;
+            return
+        }
+
+        let current = this.head;
+
+        while (current != null && current.next != null) {
+
+            current = current.next
+        }
+        current.next = newNode
+        this.size++
+    }
+
+    addAtIndex(index, val){
+
+        if(index < 0  || index > this.size) return;
+
+        let newNode = new Node(val);
+        let current = this.head
+
+        if(index == 0){
+            
+            newNode.next = current;
+            this.head = newNode;
+            this.size++
+            return
+        }
+
+        if(index === this.size){
+            while( current.next != null){
+
+                current = current.next;
+            }
+            current.next = newNode
+            this.size++
+            return
+        }
+     
+        for(let i = 0; i < index - 1; i++ ){
+            current = current.next
+          
+        }
+        newNode.next = current.next
+        current.next = newNode
+
+        this.size++
+    }
+
+}
+
+
+let list = new MyLinkedList();
+
+list.addToHead(10)
+list.addToTail(20)
+list.addToTail(40)
+list.addAtIndex(3, 50)
+
+console.log(JSON.stringify(list, null, 2));
