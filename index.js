8bfbfdb724ab9022ea309900d592d9c99f498b4a
krititwa <krititwaray97@gmail.com>
2026-03-31 21:39:38 +0530

    oddEvenList

--- index.js
+++ index.js
@@ -7,44 +7,15 @@ class ListNode {
 
 let head = null
 head = new ListNode(1)
-head.next = new ListNode(1)
-head.next.next = new ListNode(2)
-head.next.next.next = new ListNode(3)
+head.next = new ListNode(2)
+head.next.next = new ListNode(3)
+head.next.next.next = new ListNode(4)
 head.next.next.next.next = new ListNode(3)
-// head.next.next.next.next.next = new ListNode(5)
-// head.next.next.next.next.next.next = new ListNode(6)
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var deleteDuplicates = function(head) {
 
-    let current = head;
 
-    while(current.next){
-        if(current.val == current.next.val){
-            current.next = current.next.next
-        }else{
-            current = current.next
-            
-        }
-    }
-    return head
-    /* 
+const oddEvenList = function(head) {
     
-    Time Complexity: O(n)
-    Space Complexity: O(1)
-    
-    */
 };
 
-let result = deleteDuplicates(head)
-console.log(JSON.stringify(result, null, 2))
+console.log("oddEvenList")
