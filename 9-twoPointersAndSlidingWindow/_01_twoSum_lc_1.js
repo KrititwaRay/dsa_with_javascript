e5c942bc884bc540a6d2e2107dd5654eeb3089e6
krititwa <krititwaray97@gmail.com>
2026-04-28 11:46:03 +0530

    1. Two Sum

---
+++ 9-twoPointersAndSlidingWindow/_01_twoSum_lc_1.js
@@ -0,0 +1,94 @@
+/* 1. Two Sum */
+
+/* 
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 10^4
+-10^9 <= nums[i] <= 10^9
+-10^9 <= target <= 10^9
+Only one valid answer exists.
+
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+let nums =  [2,7,11,15];
+let target = 9;
+
+var twoSum_01 = function(nums, target) {
+    
+    for (let i = 0; i < nums.length - 1; i++) {
+
+        for (let j = i + 1; j < nums.length; j++) {
+            let sum = nums[i] + nums[j];
+
+            if(sum === target){
+                return [i,j]
+            }
+
+        }
+    }
+
+    /* 
+    Time Complexity: O(n^2)
+    Space Complexity: O(1)
+    
+    */
+};
+
+// let result = twoSum_01(nums, target);
+// console.log(result)
+
+
+var twoSum_02 = function(nums, target) {
+
+    let map = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        map[nums[i]] = i
+    }
+    for (let i = 0; i < nums.length; i++) {
+        let pairToFind = target - nums[i]
+        
+        if(map[pairToFind] && map[pairToFind] != i){
+            return [i, map[pairToFind]]
+        }
+       
+    }
+
+    /* 
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+    
+    */
+   
+};
+let result = twoSum_02(nums, target);
+console.log(result)
