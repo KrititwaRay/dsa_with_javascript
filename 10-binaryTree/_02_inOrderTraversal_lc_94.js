2ebc6fd62adf186d3b4be84fd96753bc08f00a93
krititwa <krititwaray97@gmail.com>
2026-05-02 20:48:48 +0530

    94. Binary Tree Inorder Traversal

--- 10-binaryTree/_02_inOrderTraversal_lc_94.js
+++ 10-binaryTree/_02_inOrderTraversal_lc_94.js
@@ -1,41 +1,36 @@
-/* 144. Binary Tree Preorder Traversal */
-
+/* 94. Binary Tree Inorder Traversal */
 
 /* 
 
-
-Given the root of a binary tree, return the preorder traversal of its nodes' values.
-
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
 Example 1:
-
 Input: root = [1,null,2,3]
-Output: [1,2,3]
+Output: [1,3,2]
+
 
 
 Example 2:
-
 Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
-Output: [1,2,4,5,6,7,3,8,9]
+Output: [4,2,6,5,7,1,3,9,8]
 
 
 Example 3:
-
 Input: root = []
 Output: []
 
-Example 4:
 
+
+Example 4:
 Input: root = [1]
 Output: [1]
 
-
+ 
 
 Constraints:
 
 The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
- 
 
 */
 
@@ -54,8 +49,8 @@ The number of nodes in the tree is in the range [0, 100].
  */
 
 
-class TreeNode{
-    constructor(val){
+class TreeNode {
+    constructor(val) {
         this.val = val;
         this.left = null;
         this.right = null;
@@ -72,45 +67,54 @@ function buildFromArray(arr) {
     while (i < arr.length) {
         let current = queue.shift();
 
-        if (arr[i] !== null) {
+        // left
+        if (arr[i] != null) {
             current.left = new TreeNode(arr[i]);
-            queue.push(current.left);
+            queue.push(current.left)
         }
-        i++;
+        i++
 
-        if (i < arr.length && arr[i] !== null) {
+
+        // right
+        if (i < arr.length && arr[i] != null) {
             current.right = new TreeNode(arr[i]);
-            queue.push(current.right);
+            queue.push(current.right)
         }
-        i++;
+        i++
     }
 
-    return root;
-}
 
+    return root;
+
+}
 
 
 let arr = [1, null, 2, 3];
 let root = buildFromArray(arr);
 
 
-/* Recursive  */
-function preorderTraversal(root) {
+
+var inorderTraversal = function(root) {
+    // left -> root -> right
 
     let ans = [];
 
-    function traversal(current) {
-        if (!current) return;
+    function traversal(current){
 
-        ans.push(current.val); 
-        traversal(current.left);
-        traversal(current.right);
+        if(!current) return
+
+        
+        traversal(current.left)
+        ans.push(current.val)
+        traversal(current.right)
+
     }
 
-    traversal(root);
+    traversal(root)
+
     return ans;
-}
+    
+};
 
-
-let result = preorderTraversal(root);
-console.log("result: ", result);
+let result = inorderTraversal(root);
+console.log(result)
--- index.js
+++ index.js
@@ -1,10 +1,69 @@
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function buildFromArray(arr) {
+    if (!arr.length) return null;
+
+    let root = new TreeNode(arr[0]);
+    let queue = [root];
+    let i = 1;
+
+    while (i < arr.length) {
+        let current = queue.shift();
+
+        // left
+        if (arr[i] != null) {
+            current.left = new TreeNode(arr[i]);
+            queue.push(current.left)
+        }
+        i++
+
+
+        // right
+        if (i < arr.length && arr[i] != null) {
+            current.right = new TreeNode(arr[i]);
+            queue.push(current.right)
+        }
+        i++
+    }
+
+
+    return root;
+
+}
+
+
+let arr = [1, null, 2, 3];
+let root = buildFromArray(arr);
 
 
 
+var inorderTraversal = function(root) {
+    // left -> root -> right
 
+    let ans = [];
 
+    function traversal(current){
 
+        if(!current) return
 
+        
+        traversal(current.left)
+        ans.push(current.val)
+        traversal(current.right)
 
+    }
 
+    traversal(root)
 
+    return ans;
+    
+};
+
+let result = inorderTraversal(root);
+console.log(result)
