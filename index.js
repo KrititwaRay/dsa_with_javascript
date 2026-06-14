546e470a144fbb384ff23e76c9cdb4b40904537f
krititwa <krititwaray97@gmail.com>
2026-06-14 11:05:29 +0530

    167. Two Sum II - Input Array Is Sorted

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let nums =  [2,7,11,15];
+let target = 9;
 
 
+let map = new Map();
+
+
+
+for(let i = 0; i < nums.length; i++){
+    map.set(nums[i], i)
+}
+
+
+for(let i = 0; i< nums.length; i++){
+    let pairToFind = target - nums[i]
+
+    
+
+    if (map.get(pairToFind) && map.get(pairToFind) != i){
+        return [map.get(pairToFind), i]
+    }
+}
